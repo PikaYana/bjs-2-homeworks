e2f97a5be09c47fac0dce772c41b3d0164c1038d
Pika Yana <pikayana611@gmail.com>
2023-04-15 01:51:06 +0300

    Первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,34 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = b**2 - 4*a*c;
+
+  if(d > 0) {
+    arr.push((-b + Math.sqrt(d) )/(2*a));
+    arr.push((-b - Math.sqrt(d) )/(2*a));
+  }
+
+  else if(d === 0) {
+    arr.push(-b/(2*a));
+  }
+return arr;
+  else {
+    arr = [];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if(isNaN(percent) isNaN(contribution) isNaN(amount) isNaN(countMonths)) {
+    return false;
+  }
+
+  percent = percent/100/12;
+
+  let loanBody = amount - contribution;
+
+  let monthlyPayment = loanBody*(percent + (percent/((1 + percent)**countMonths - 1)));
+
+  let totalAmount = monthlyPayment*countMonths;
+
+  return Number(totalAmount.toFixed(2));
+
 }
