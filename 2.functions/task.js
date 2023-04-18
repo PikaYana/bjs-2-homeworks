f2b1fb85de34b36b9a6fb6258c1ac7c84999d00e
Pika Yana <pikayana611@gmail.com>
2023-04-18 09:50:22 +0300

    Второе задание задача1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,23 @@
 function getArrayParams(...arr) {
+let max = 6;
+let min = 3;
+let avg = 4;
+let sum = 0;
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  }
+}
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > min) {
+    min = arr[i];
+    }
+  }
+for (let i = 1; i < arr.length; i++) {
+  sum += arr[i];
+  avg = sum /arr.length;
+  Number(avg.toFixed(2));
+}
 
   return { min: min, max: max, avg: avg };
 }
