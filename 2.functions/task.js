f1c74b96d8261b665fcc3ecd90f389e52d2bd3d4
Pika Yana <pikayana611@gmail.com>
2023-04-24 14:53:43 +0300

    Второе задание: исправленна 1 задача и решена 2 задача

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,41 +1,72 @@
 function getArrayParams(...arr) {
-let max = 6;
-let min = 3;
-let avg = 4;
+let max = 0;
+let min = 0;
+let avg = sum/arr.length;
 let sum = 0;
-for (let i = 1; i < arr.length; i++) {
+for (let i = 0; i < arr.length; i++) {
   if (arr[i] > max) {
     max = arr[i];
   }
-}
-for (let i = 1; i < arr.length; i++) {
   if (arr[i] > min) {
     min = arr[i];
     }
-  }
-for (let i = 1; i < arr.length; i++) {
-  sum += arr[i];
-  avg = sum /arr.length;
-  Number(avg.toFixed(2));
+  if (sum += arr[i]) {
+    avg = Number(avg.toFixed(2));
 }
-
+}
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
+console.log(summElementsWorker()); 
+console.log(summElementsWorker(10, 10, 11, 20, 10));
 
 function differenceMaxMinWorker(...arr) {
-
+  let max;
+  let min;
+  for(let i = 0; i < arr.length; i++ ) {
+    if (arr[i] > max) {
+    max = arr[i];
+    }
+    if (arr[i] > min) {
+    min = arr[i];
+    }
+  }
+  return max - min;
 }
+console.log(differenceMaxMinWorker());
+console.log(differenceMaxMinWorker(10, 10, 11, 20, 10));
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    }
+    else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let countEvenElement =0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 ===0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return sumEvenElement/countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
